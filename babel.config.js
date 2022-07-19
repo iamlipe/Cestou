7d639728bd06b3167a2e3c77a56458059908351c
Felipe Lima <felipelima@ioasys.com.br>
2022-07-19 10:57:43 -0300

    feat config babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,27 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@__mocks__': './__mocks__',
+          '@__tests__': './__tests__',
+        },
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.json',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+        ],
+      },
+    ],
+    ['module:react-native-dotenv', {moduleName: 'react-native-dotenv'}],
+    'react-native-reanimated/plugin',
+  ],
 };
