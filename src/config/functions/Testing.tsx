e3bc144874ccdf1ec0d99fb290988a51a34578e5
Felipe Lima <felipelima@ioasys.com.br>
2022-07-19 19:16:31 -0300

    fix render whith provider adding store

--- src/config/functions/Testing.tsx
+++ src/config/functions/Testing.tsx
@@ -1,11 +1,17 @@
 import React, {ReactNode} from 'react';
 import renderer from 'react-test-renderer';
 import {theme} from '@/styles';
+import {Provider} from 'react-redux';
 import {ThemeProvider} from 'styled-components/native';
 import {render} from '@testing-library/react-native';
+import {store} from '@/store';
 
 const wraperWithProvider = (children: ReactNode) => {
-  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>{children}</ThemeProvider>
+    </Provider>
+  );
 };
 
 export const renderWithThemeProvider = (component: ReactNode) => {
