7ef5b70efb156e83c4986f9560ffcb72bdf6aaf0
Felipe Lima <felipelima@ioasys.com.br>
2022-07-19 19:17:57 -0300

    test screen login

---
+++ __tests__/presentational/LoginScreen/Login.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {act, fireEvent} from '@testing-library/react-native';
+import {
+  renderWithThemeProvider,
+  matchSnapshotWithProvider,
+} from '@/config/functions/testing';
+
+import {Login} from '@/presentational/LoginScreen/Login';
+
+describe('Login', () => {
+  test('should render screen correctly', () => {
+    renderWithThemeProvider(<Login />);
+  });
+
+  test('should matches snapshot', () => {
+    matchSnapshotWithProvider(<Login />);
+  });
+
+  test('submit form', async () => {
+    const {getByTestId, getByText} = renderWithThemeProvider(<Login />);
+
+    const emailInput = getByTestId('input-text-phoneOrEmail');
+    const passwordInput = getByTestId('input-text-password');
+
+    // TODO: reevaluate mock react-hook-form
+    fireEvent.changeText(emailInput, 'caguei');
+    fireEvent.changeText(passwordInput, 'caguei');
+
+    await act(() => fireEvent.press(getByText(/entrar/i)));
+
+    expect(emailInput.props.value).toEqual('caguei');
+    expect(passwordInput.props.value).toEqual('caguei');
+  });
+
+  test('should render Logo', () => {
+    const {getByTestId} = renderWithThemeProvider(<Login />);
+
+    const logo = getByTestId('logo');
+
+    expect(logo).toBeTruthy();
+  });
+});
