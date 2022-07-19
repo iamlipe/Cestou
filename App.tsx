5bea7e77c588776f029a8aa8c0ee6f85a18be8da
Felipe Lima <felipelima@ioasys.com.br>
2022-07-19 10:09:03 -0300

    init

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>CESTOU</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
