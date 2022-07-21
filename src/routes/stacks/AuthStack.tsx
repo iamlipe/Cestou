033b725850091b66f6fd95cbf7bb62c69164e6b7
Felipe Lima <felipelima@ioasys.com.br>
2022-07-21 17:43:34 -0300

    feat auth route

---
+++ src/routes/stacks/AuthStack.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {Login} from '@/presentational/LoginScreen/Login';
+import {Register} from '@/presentational/LoginScreen/Register';
+import {ConfirmRegister} from '@/presentational/LoginScreen/ConfirmRegister';
+import {Onboarding} from '@/presentational/OnboardingScreen/Onboarding';
+
+export type AuthStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  ConfirmRegister: undefined;
+  Onboarding: undefined;
+};
+
+const Auth = createNativeStackNavigator<AuthStackParamList>();
+
+export const AuthStack = () => (
+  <Auth.Navigator screenOptions={{headerShown: false}}>
+    <Auth.Screen name="Login" component={Login} />
+    <Auth.Screen name="Register" component={Register} />
+    <Auth.Screen name="ConfirmRegister" component={ConfirmRegister} />
+    <Auth.Screen name="Onboarding" component={Onboarding} />
+  </Auth.Navigator>
+);
