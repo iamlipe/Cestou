c02b24911061f399f6e11211d1b6bd3c41a62c3b
Felipe Lima <felipelima@ioasys.com.br>
2022-07-21 15:15:12 -0300

    feat screen onboardin done

--- src/presentational/OnboardingScreen/Onboarding.tsx
+++ src/presentational/OnboardingScreen/Onboarding.tsx
@@ -1,13 +1,150 @@
 import React from 'react';
-import styled from 'styled-components/native';
+import styled, {useTheme} from 'styled-components/native';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import {ImageSourcePropType} from 'react-native';
+
+import imgOnboardinOne from '@/assets/images/onboarding-1.png';
+import imgOnboardinTwo from '@/assets/images/onboarding-2.png';
+import imgOnboardinThree from '@/assets/images/onboarding-3.png';
+
+import Button from '@/components/Button';
+
+interface SlideProps {
+  key: string;
+  title: string;
+  text: string;
+  image: ImageSourcePropType;
+}
+
+const slides: SlideProps[] = [
+  {
+    key: 'one',
+    title: 'Tire a fome do caminho',
+    text: 'Alimentação é direito básico e a sua partilha é fundamental à quem tem fome. Através da assinatura mensal, você garante alimentação saudável democratizada pra sua família e ainda possibilita o mesmo consumo pra uma família em vulnerabilidade.',
+    image: imgOnboardinOne,
+  },
+  {
+    key: 'two',
+    title: 'Agricultura familiar presente',
+    text: 'Conecte-se com produtores familiares da sua região. Você fortalece a produção de pequenos produtores locais e recebe alimentos frescos e cultivados com muito carinho e cuidado.',
+    image: imgOnboardinTwo,
+  },
+  {
+    key: 'three',
+    title: 'Alimentação saudável ao seu alcance',
+    text: 'Aqui você encontra alimentos totalmente livres de agrotóxicos e perto de você!',
+    image: imgOnboardinThree,
+  },
+];
 
 export const Onboarding = () => {
-  return (
-    <StyledContainer>
-      <StyledText>Onboarding</StyledText>
+  const theme = useTheme();
+
+  const renderSlide = ({item}: {item: SlideProps}) => (
+    <StyledContainer key={item.key} showsVerticalScrollIndicator={false}>
+      <StyledImage source={item.image} resizeMode="stretch" />
+      <StyledTitle>{item.title}</StyledTitle>
+      <StyledText>{item.text}</StyledText>
     </StyledContainer>
+  );
+
+  const renderDone = () => (
+    <Button title="Finalizar" size="medium" onPress={() => null} />
+  );
+
+  const renderNext = () => (
+    <StyledButtonNext>
+      <StyledButtonTextNext>Próximo</StyledButtonTextNext>
+    </StyledButtonNext>
+  );
+
+  const renderSkip = () => (
+    <StyledButtonSkip>
+      <StyledButtonTextSkip>Pular</StyledButtonTextSkip>
+    </StyledButtonSkip>
+  );
+
+  const renderPrev = () => (
+    <StyledButtonPrev>
+      <StyledButtonTextPrev>Voltar</StyledButtonTextPrev>
+    </StyledButtonPrev>
+  );
+
+  return (
+    <AppIntroSlider
+      data={slides}
+      renderItem={renderSlide}
+      showPrevButton
+      showSkipButton
+      activeDotStyle={{
+        backgroundColor: theme.colors.PRIMARY_800,
+        width: 6,
+        height: 6,
+      }}
+      dotStyle={{
+        backgroundColor: theme.colors.PRIMARY_400,
+        width: 6,
+        height: 6,
+      }}
+      renderDoneButton={renderDone}
+      renderNextButton={renderNext}
+      renderPrevButton={renderPrev}
+      renderSkipButton={renderSkip}
+    />
   );
 };
 
-const StyledContainer = styled.View``;
-const StyledText = styled.Text``;
+const StyledContainer = styled.ScrollView`
+  min-height: 100%;
+  background-color: ${({theme}) => theme.colors.BACKGROUND};
+`;
+
+const StyledImage = styled.Image`
+  width: 100%;
+`;
+
+const StyledTitle = styled.Text`
+  font-family: ${({theme}) => theme.fonts.SEMIBOLD_SOURCESANSPRO};
+  font-size: ${({theme}) => theme.sizing.LARGER};
+  color: ${({theme}) => theme.colors.PRIMARY_800};
+  text-align: center;
+  margin-top: 72px;
+`;
+
+const StyledText = styled.Text`
+  width: 90%;
+  align-self: center;
+  font-family: ${({theme}) => theme.fonts.REGULAR_SOURCESANSPRO};
+  font-size: ${({theme}) => theme.sizing.SMALLER};
+  color: ${({theme}) => theme.colors.GRAY_900};
+  text-align: center;
+  margin-top: 24px;
+  margin-bottom: 120px;
+`;
+
+const StyledButtonNext = styled.View`
+  background-color: ${({theme}) => theme.colors.PRIMARY_800};
+  border-radius: 5px;
+  height: 38px;
+  width: 90px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledButtonTextNext = styled.Text`
+  font-family: ${({theme}) => theme.fonts.REGULAR_SOURCESANSPRO};
+  font-size: ${({theme}) => theme.sizing.SMALLER};
+  color: ${({theme}) => theme.colors.WHITE};
+`;
+
+const StyledButtonPrev = styled(StyledButtonNext)`
+  background-color: ${({theme}) => theme.colors.WHITE};
+`;
+
+const StyledButtonTextPrev = styled(StyledButtonTextNext)`
+  color: ${({theme}) => theme.colors.PRIMARY_800};
+`;
+
+const StyledButtonSkip = styled(StyledButtonPrev)``;
+
+const StyledButtonTextSkip = styled(StyledButtonTextPrev)``;
