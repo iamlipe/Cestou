1ec1216db42f60b9aa8c86ea55d863efe552d72a
Felipe Lima <felipelima@ioasys.com.br>
2022-07-22 16:52:03 -0300

    feat add api interceptors

--- src/config/services/api.ts
+++ src/config/services/api.ts
@@ -3,4 +3,23 @@ import {API_URL} from 'react-native-dotenv';
 
 const api = axios.create({baseURL: API_URL});
 
+api.interceptors.response.use(
+  response => response,
+  async error => {
+    const originalRequest = error.config;
+
+    if (
+      error.response.status === 401 &&
+      originalRequest.url !== '/auth/refresh' &&
+      !originalRequest.retry
+    ) {
+      originalRequest.retry = true;
+      await api.put('/auth/refresh');
+      return api(originalRequest);
+    }
+
+    return Promise.reject(error);
+  },
+);
+
 export default api;
