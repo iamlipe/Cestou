6b15660e02b8277f2f6f2eb7ac3e9ee8b801aaf8
Felipe Lima <felipelima@ioasys.com.br>
2022-07-28 13:46:49 -0300

    feat export produce and basket slice

--- src/store/slices/index.ts
+++ src/store/slices/index.ts
@@ -1,10 +1,16 @@
 import {combineReducers} from '@reduxjs/toolkit';
 
 import user, {userState} from './userSlice';
+import producer, {producerState} from './producerSlice';
+import basket, {basketState} from './basketSlice';
 
-export const globalState = {user: userState};
+export const globalState = {
+  user: userState,
+  producer: producerState,
+  basket: basketState,
+};
 
-export const globalReducer = {user};
+export const globalReducer = {user, producer, basket};
 
 const rootReducer = combineReducers(globalReducer);
 
