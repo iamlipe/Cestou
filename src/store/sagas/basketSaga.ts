52091ceb0607a08e96898af7fa74ffb23e7b108e
Felipe Lima <felipelima@ioasys.com.br>
2022-07-28 13:47:11 -0300

    feat basket saga

---
+++ src/store/sagas/basketSaga.ts
@@ -0,0 +1,49 @@
+import api from '@/config/services/api';
+import {PayloadAction} from '@reduxjs/toolkit';
+import {AxiosResponse} from 'axios';
+import {takeLatest, all, put, call} from 'redux-saga/effects';
+
+import {
+  BasketResponse,
+  GET_BASKET,
+  GET_BASKET_FAILURE,
+  GET_BASKET_SUCCESS,
+  SignupProducerBasketRequest,
+  SIGNUP_PRODUCER_BASKET,
+  SIGNUP_PRODUCER_BASKET_FAILURE,
+  SIGNUP_PRODUCER_BASKET_SUCCESS,
+} from '../slices/basketSlice';
+
+export function* getBaskets() {
+  try {
+    const {data, status}: AxiosResponse<BasketResponse[]> = yield call(
+      api.get,
+      '/baskets/list/',
+    );
+
+    const allBaskets: BasketResponse[] = data;
+
+    yield put(GET_BASKET_SUCCESS({allBaskets, status}));
+  } catch (error) {
+    yield put(GET_BASKET_FAILURE({error}));
+  }
+}
+
+export function* signupProducerBasket({
+  payload,
+}: PayloadAction<SignupProducerBasketRequest>) {
+  try {
+    yield call(api.post, '/producers/signup-producer-basket', payload);
+
+    yield put(SIGNUP_PRODUCER_BASKET_SUCCESS());
+  } catch (error) {
+    yield put(SIGNUP_PRODUCER_BASKET_FAILURE({error}));
+  }
+}
+
+export default function* watcher() {
+  yield all([
+    takeLatest(GET_BASKET, getBaskets),
+    takeLatest(SIGNUP_PRODUCER_BASKET, signupProducerBasket),
+  ]);
+}
