21260f52927969831aa0c67f4a2b8fedccb4485f
Felipe Lima <felipelima@ioasys.com.br>
2022-08-02 13:40:40 -0300

    feat screen profile

--- src/presentational/ProfileScreen/Profile.tsx
+++ src/presentational/ProfileScreen/Profile.tsx
@@ -1,17 +1,52 @@
 import React from 'react';
-import styled from 'styled-components/native';
+import styled, {useTheme} from 'styled-components/native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import {LOGOUT} from '@/store/slices/userSlice';
 import {useReduxDispatch} from '@/hooks/useReduxDispatch';
 
+import Header from '@/components/Header';
+import ButtonRedirect from '@/components/ButtonRedirect';
+
 export const Profile = () => {
   const dispatch = useReduxDispatch();
+  const theme = useTheme();
 
   return (
-    <StyledContainer>
-      <StyledButton title="Sair" onPress={() => dispatch(LOGOUT())} />
-    </StyledContainer>
+    <StyledContainerScroll showsVerticalScrollIndicator={false}>
+      <Header title="Perfil" type="profile" welcome={false} />
+      <StyledContent>
+        <ButtonRedirect title="Meus dados" onPress={() => null} />
+        <ButtonRedirect title="Minhas cestas" onPress={() => null} />
+
+        <StyledLogoutButton onPress={() => dispatch(LOGOUT())}>
+          <StyledTitleLogoutButton>Sair da conta</StyledTitleLogoutButton>
+
+          <Icon name="logout" size={20} color={theme.colors.GRAY_900} />
+        </StyledLogoutButton>
+      </StyledContent>
+    </StyledContainerScroll>
   );
 };
 
-const StyledContainer = styled.View``;
-const StyledButton = styled.Button``;
+const StyledContainerScroll = styled.ScrollView`
+  min-height: 100%;
+  background-color: ${({theme}) => theme.colors.BACKGROUND};
+`;
+
+const StyledContent = styled.View`
+  width: 90%;
+  align-self: center;
+  padding: 24px 0;
+`;
+
+const StyledLogoutButton = styled.TouchableOpacity`
+  width: 120px;
+  flex-direction: row;
+  align-items: center;
+
+  margin-top: 100px;
+`;
+
+const StyledTitleLogoutButton = styled.Text`
+  margin-right: 10px;
+`;
