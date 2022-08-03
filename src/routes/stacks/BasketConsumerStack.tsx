2a5abce27063a2afd590b5564a4cc5bf9d32e9ef
Felipe Lima <felipelima@ioasys.com.br>
2022-08-03 11:26:22 -0300

    feat add new stack to basket consumer

---
+++ src/routes/stacks/BasketConsumerStack.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import {BasketSignupPlanConsumer} from '@/presentational/BasketScreen/BasketSignupPlanConsumer';
+import {BasketSignupFoodConsumer} from '@/presentational/BasketScreen/BasketSignupFoodConsumer';
+import {BasketSignupPaymentConsumer} from '@/presentational/BasketScreen/BasketSignupPaymentConsumer';
+
+export type BasketConsumerStackParamList = {
+  BasketSignupPlanConsumer: undefined;
+  BasketSignupFoodConsumer: {daysPerDeliver: string; size: string};
+  BasketSignupPaymentConsumer: undefined;
+};
+
+const BasketConsumer =
+  createNativeStackNavigator<BasketConsumerStackParamList>();
+
+export const BasketConsumerStack = () => {
+  return (
+    <BasketConsumer.Navigator
+      initialRouteName="BasketSignupPlanConsumer"
+      screenOptions={{
+        headerShown: false,
+        animation: 'slide_from_right',
+      }}>
+      <BasketConsumer.Screen
+        name="BasketSignupPlanConsumer"
+        component={BasketSignupPlanConsumer}
+      />
+      <BasketConsumer.Screen
+        name="BasketSignupFoodConsumer"
+        component={BasketSignupFoodConsumer}
+      />
+      <BasketConsumer.Screen
+        name="BasketSignupPaymentConsumer"
+        component={BasketSignupPaymentConsumer}
+      />
+    </BasketConsumer.Navigator>
+  );
+};
