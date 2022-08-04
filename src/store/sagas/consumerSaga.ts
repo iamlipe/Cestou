996e668f22827e54bea6cbe96a15031e040b6ce9
Felipe Lima <felipelima@ioasys.com.br>
2022-08-04 18:02:47 -0300

    feat consumer saga get consumer basket

---
+++ src/store/sagas/consumerSaga.ts
@@ -0,0 +1,29 @@
+import api from '@/config/services/api';
+import {AxiosResponse} from 'axios';
+import {takeLatest, all, put, call} from 'redux-saga/effects';
+
+import {
+  ConsumerBasketResponse,
+  GET_CONSUMER_BASKET,
+  GET_CONSUMER_BASKET_FAILURE,
+  GET_CONSUMER_BASKET_SUCCESS,
+} from '../slices/consumerSlice';
+
+export function* getConsumerBasket() {
+  try {
+    const {data, status}: AxiosResponse<ConsumerBasketResponse> = yield call(
+      api.get,
+      `/consumers/get-consumer-basket`,
+    );
+
+    console.log(data);
+
+    yield put(GET_CONSUMER_BASKET_SUCCESS({data, status}));
+  } catch (error) {
+    yield put(GET_CONSUMER_BASKET_FAILURE({error}));
+  }
+}
+
+export default function* watcher() {
+  yield all([takeLatest(GET_CONSUMER_BASKET, getConsumerBasket)]);
+}
