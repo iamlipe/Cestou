904775f184eb3f9643c17eec64c2ff2c3f892396
Felipe Lima <felipelima@ioasys.com.br>
2022-08-10 10:51:23 -0300

    feat add new route splash

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  NavigatorScreenParams,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {AppStack} from './stacks/AppStack';
+import {AppStack, AppStackParamList} from './stacks/AppStack';
+import {Splash} from '@/presentational/SplashScreen/Splash';
 
 export type RoutesParamList = {
-  AppStack: undefined;
+  AppStack: NavigatorScreenParams<AppStackParamList> | undefined;
+  Splash: undefined;
 };
 
 const MainStack = createNativeStackNavigator<RoutesParamList>();
@@ -13,11 +18,12 @@ const MainStack = createNativeStackNavigator<RoutesParamList>();
 export const Routes = () => (
   <NavigationContainer>
     <MainStack.Navigator
-      initialRouteName="AppStack"
+      initialRouteName="Splash"
       screenOptions={{
         headerShown: false,
         gestureEnabled: false,
       }}>
+      <MainStack.Screen name="Splash" component={Splash} />
       <MainStack.Screen name="AppStack" component={AppStack} />
     </MainStack.Navigator>
   </NavigationContainer>
