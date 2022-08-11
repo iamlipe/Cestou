3e142258768ec9c41db520f2eaa0e4210b49b176
Felipe Lima <felipelima@ioasys.com.br>
2022-08-11 20:23:56 -0300

    feat config i18next

---
+++ src/config/i18n/index.ts
@@ -0,0 +1,15 @@
+import i18next from 'i18next';
+import {initReactI18next} from 'react-i18next';
+
+import pt from './pt.json';
+import en from './en.json';
+
+i18next.use(initReactI18next).init({
+  compatibilityJSON: 'v3',
+  resources: {pt, en},
+  lng: 'pt',
+  react: {useSuspense: false},
+  interpolation: {escapeValue: false},
+});
+
+export default i18next;
