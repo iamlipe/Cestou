f3c3f6fbfce9947518dc5f19878abef8826fdb76
Felipe Lima <felipelima@ioasys.com.br>
2022-08-11 20:25:49 -0300

    fix helper getdefault params

--- src/helpers/getDefaulParams.ts
+++ src/helpers/getDefaulParams.ts
@@ -1,5 +1,5 @@
 export const getDefaulSize = (size: string) => {
-  if (size.startsWith('P')) {
+  if (size.startsWith('P') || size.startsWith('S')) {
     return 'small';
   }
 
@@ -7,17 +7,17 @@ export const getDefaulSize = (size: string) => {
     return 'medium';
   }
 
-  if (size.startsWith('G')) {
+  if (size.startsWith('G') || size.startsWith('L')) {
     return 'big';
   }
 };
 
 export const getDefaultDeliver = (daysPerDeliver: string) => {
-  if (daysPerDeliver.startsWith('S')) {
+  if (daysPerDeliver.startsWith('S') || daysPerDeliver.startsWith('W')) {
     return '7';
   }
 
-  if (daysPerDeliver.startsWith('Q')) {
+  if (daysPerDeliver.startsWith('Q') || daysPerDeliver.startsWith('F')) {
     return '15';
   }
 };
