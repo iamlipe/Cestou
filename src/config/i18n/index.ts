15fdde043a47ab5e42d91a6ed45d29f8766764c7
Felipe Lima <felipelima@ioasys.com.br>
2022-08-12 16:52:20 -0300

    feat get best language

--- src/config/i18n/index.ts
+++ src/config/i18n/index.ts
@@ -1,4 +1,5 @@
 import i18next from 'i18next';
+import * as RNLocalize from 'react-native-localize';
 import {initReactI18next} from 'react-i18next';
 
 import pt from './pt.json';
@@ -7,7 +8,7 @@ import en from './en.json';
 i18next.use(initReactI18next).init({
   compatibilityJSON: 'v3',
   resources: {pt, en},
-  lng: 'en',
+  lng: RNLocalize.findBestAvailableLanguage(['en', 'pt'])?.languageTag,
   react: {useSuspense: false},
   interpolation: {escapeValue: false},
 });
